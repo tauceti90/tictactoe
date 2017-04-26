90cf1830b6eec4ee698e659e0cbef20403293876
Alex Mathew <alexmathewphilip@gmail.com>
2017-04-25 21:25:24 -0500

    Added class representing board.

---
+++ TicTacToe/Board.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <array>
+
+/**
+Class representing Tic-Tac-Toe 'board'
+*/
+class Board
+{
+private:
+	char board_[9]; // I can't think of a tic-tac-toe game thats not 3 x 3
+};
